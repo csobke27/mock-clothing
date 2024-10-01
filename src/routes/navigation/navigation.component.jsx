4fb904b3ef2999b0948f173483910aca32130d54
csobke27 <csobke@gmail.com>
2024-09-30 20:10:45 -0400

    adding cart functionality to navigation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,11 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import {UserContext} from '../../contexts/user.context';
+import { CartContext } from "../../contexts/cart.context";
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
@@ -10,7 +14,7 @@ import './navigation.styles.scss';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
-
+    const { isCartOpen } = useContext(CartContext);
 
     return (
         <Fragment>
@@ -31,8 +35,9 @@ const Navigation = () => {
                         <Link className="nav-link" to='/auth'>SIGN IN</Link>
                     )
                     }
-                    
+                    <CartIcon />
                 </div>
+                {isCartOpen && <CartDropdown />}
             </div>
             <Outlet />
         </Fragment>
