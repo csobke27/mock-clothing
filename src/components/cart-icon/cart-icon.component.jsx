0b51757ba9aa63cb1df26ba8a2910a20dd157b1f
csobke27 <csobke@gmail.com>
2024-09-30 21:32:55 -0400

    added cart functionality

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,16 +6,15 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
 
-const CartIcon = () => {
-    const items = [];
-
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+const CartIcon = () => {   
+    const { isCartOpen, setIsCartOpen, cartItems, cartCount } = useContext(CartContext);
 
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
     return (
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingIcon className="shopping-icon" />
-            <span className='item-count'>{items.length}</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     );
 };
